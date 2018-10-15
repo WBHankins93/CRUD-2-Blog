52f8ec400644005e06a29df1dd1035942d6988fc
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-10-15 11:28:42 -0500

    new page created and route added

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const Author = require('../model/authors')
+const Author = require('../model/authors');
 
 router.get('/', (req, res) => {
   res.render('authors/index.ejs');
+})
+
+router.get('/new', (req, res) => {
+  res.render('/authors/new.ejs');
 })
 
 module.exports = router;
