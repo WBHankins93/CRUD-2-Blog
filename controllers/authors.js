526442b46085ca7c742e29b29ff756e1ce2a1ad4
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-10-15 11:56:40 -0500

    created show page and route to show page for authors

--- controllers/authors.js
+++ controllers/authors.js
@@ -14,6 +14,14 @@ router.get('/new', (req, res) => {
   res.render('/authors/new.ejs');
 })
 
+router.get('/:id', (req, res) => {
+  Author.findById(req.params.id, (err, authorFound) => {
+    res.render('authors/show.ejs', {
+      author: authorFound
+    })
+  })
+})
+
 router.post('/', (req, res) => {
   Author.create(req.body, (err, createdAuthor) => {
     console.log(req.body, 'this is req.body');
